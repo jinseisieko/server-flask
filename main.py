3f46731a404a8dc27e9fe3ea6ad0dccc137b6ea8
zeta <akitousira@yandex.ru>
2024-03-13 18:15:39 +0300

    click

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import argparse
+from typing import IO
 
+import click
 import requests
 from pprint import pprint
 
-parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
 
-parser.add_argument('filename')           # positional argument
-parser.add_argument('-c', '--count', type=int)      # option that takes a value
-parser.add_argument('-v', '--verbose',
-                    action='store_true')  # on/off flag
+@click.command(epilog="Text at the bottom of help")
+@click.argument('filename', type=click.File())
+@click.option('-c', '--count', default=1, type=int, help="Number")
+@click.option('-v', '--verbose', is_flag=True, help="Flag")
+def main(filename: IO, count, verbose):
+    """What the program does"""
+    click.echo((filename.read(), count, verbose))
 
-args = parser.parse_args()
-print(args.filename, args.count, args.verbose)
+
+if __name__ == '__main__':
+    main()
+#
+# parser = argparse.ArgumentParser(
+#     prog='ProgramName',
+#     description='What the program does',
+#     epilog='Text at the bottom of help')
+#
+# parser.add_argument('filename')  # positional argument
+# parser.add_argument('-c', '--count', type=int)  # option that takes a value
+# parser.add_argument('-v', '--verbose',
+#                     action='store_true')  # on/off flag
+#
+# args = parser.parse_args()
+# print(args.filename, args.count, args.verbose)
